17f81c936cb60eaf8ef71709145917156c27b889
Leonardo <leodev00@outlook.com>
2026-04-04 00:29:23 -0300

    Corrections and optimization of the password  hashing function

--- src/utils/hash-password.ts
+++ src/utils/hash-password.ts
@@ -1,10 +1,20 @@
 import argon2 from "argon2";
 
-export async function hashPassword(password: string) {
-  return argon2.hash(password, {
-    type: argon2.argon2id,
-    memoryCost: 65536,
-    timeCost: 3,
-    parallelism: 1,
-  });
-}
+const ARGON_OPTIONS = {
+  type: argon2.argon2id,
+  memoryCost: 65536,
+  timeCost: 3,
+  parallelism: 1,
+};
+
+export const hashPassword = async (password: string): Promise<string> => {
+  try {
+    return await argon2.hash(password, ARGON_OPTIONS);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Erro ao processar credenciais.", error);
+      throw new Error("Erro ao processar credenciais.");
+    }
+    throw new Error("Ocorreu um erro desconhecido.");
+  }
+};
